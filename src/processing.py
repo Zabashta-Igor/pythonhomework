57cb7da326fa0fea709ec42c527fc6ba5c05ec40
Игорь <Zabashtaigor@gmail.com>
2025-03-23 23:23:02 +0300

    Создали функцию filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,32 @@
+
+def filter_by_state(data, state='EXECUTED'):
+    """Функция фильтрует список словарей по указанному значению ключа 'state'. По умолчанию state == EXECUTED"""
+
+    return [item for item in data if item.get('state') == state]
+
+
+# Пример входных данных
+data = [
+{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Примеры использования функции
+# Фильтрация по умолчанию (state='EXECUTED')
+print(filter_by_state(data, state='EXECUTED'))
+# Вывод:
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+# Фильтрация по state='CANCELED'
+print(filter_by_state(data, state='CANCELED'))
+# Вывод:
+# [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def sort_by_date():
+
+    pass

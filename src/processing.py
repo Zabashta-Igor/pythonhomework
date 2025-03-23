8efd6b6e2d786a08e217e34da97d119ed62c6cf2
Игорь <Zabashtaigor@gmail.com>
2025-03-24 00:42:40 +0300

    прогресс домашней работы 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,10 @@
-
-def filter_by_state(operations: list, state: str='EXECUTED') -> list:
+def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
     """Функция фильтрует список словарей по указанному значению ключа 'state'. По умолчанию state == EXECUTED"""
 
-    return [item for item in operations if item.get('state') == state]
+    return [item for item in operations if item.get("state") == state]
 
 
 def sort_by_date(operations: list) -> list:
     """Функция сортирует список словарей, задающий порядок сортировки (по умолчанию — убывание)"""
 
-    return sorted(operations, key=lambda item: item["date"], reverse = True)
-
+    return sorted(operations, key=lambda item: item["date"], reverse=True)

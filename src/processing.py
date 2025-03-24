d09a0496332b894379c5eea56e456b811dc2e1fc
Игорь <Zabashtaigor@gmail.com>
2025-03-24 18:26:26 +0300

    исправили ошибки и настроили flake8

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,15 @@
-def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
+from typing import Any
+from typing import Dict
+from typing import List
+
+
+def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция фильтрует список словарей по указанному значению ключа 'state'. По умолчанию state == EXECUTED"""
 
     return [item for item in operations if item.get("state") == state]
 
 
-def sort_by_date(operations: list) -> list:
+def sort_by_date(operations: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     """Функция сортирует список словарей, задающий порядок сортировки (по умолчанию — убывание)"""
 
     return sorted(operations, key=lambda item: item["date"], reverse=True)

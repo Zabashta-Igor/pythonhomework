97c2a88ce2216107b9d63016393c5d10e494af8f
Игорь <Zabashtaigor@gmail.com>
2025-03-30 18:27:10 +0300

    Создан test_widgit.py и conftest.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,8 +3,8 @@ import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number(7000792289606361) == '7000 79** **** 6361'
+def test_get_mask_card_number(number_card):
+    assert get_mask_card_number(number_card) == '7000 79** **** 6361'
 
     with pytest.raises(TypeError):
         assert get_mask_card_number()
@@ -12,8 +12,8 @@ def test_get_mask_card_number():
     assert get_mask_card_number(234562) == None
 
 
-def test_get_mask_account():
-    assert get_mask_account(73654108430135874305) == '**4305'
+def test_get_mask_account(account_number):
+    assert get_mask_account(account_number) == '**4305'
 
     with pytest.raises(TypeError):
         assert get_mask_account()

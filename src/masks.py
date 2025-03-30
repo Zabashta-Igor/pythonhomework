369f71551e0e1e0aadaf1ad03787d21cf4ed56e6
Игорь <Zabashtaigor@gmail.com>
2025-03-30 17:20:24 +0300

    Создан test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,14 @@
-from typing import Union
-
-
-def get_mask_card_number(card_number: Union[str]) -> Union[str, None]:
+def get_mask_card_number(card_number: str) -> str | None:
     """Принимает номер карты и возвращает номер карты с частично замененными цифрами на *"""
 
     str_card_number = str(card_number)
-    if card_number.isdigit() and len(card_number) == 16:
+    if str_card_number.isdigit() and len(str_card_number) == 16:
         return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[12:]}"
     else:
         return None
 
 
-def get_mask_account(account_number: Union[str]) -> Union[str, None]:
+def get_mask_account(account_number: str) -> str | None:
     """Принимает номер счета и возвращает последние четыре цифры"""
 
     str_account_number = str(account_number)
@@ -21,5 +18,5 @@ def get_mask_account(account_number: Union[str]) -> Union[str, None]:
         return None
 
 
-print(get_mask_card_number("8888888888888888"))
-print(get_mask_account("88888888888888888888"))
+#print(get_mask_card_number(7000792289606361))
+#print(get_mask_account(73654108430135874305))

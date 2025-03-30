80459fe4fabf8122c36fc93cd827d75cb4952a96
Игорь <Zabashtaigor@gmail.com>
2025-03-30 19:17:41 +0300

    Прогрес домашки 10.2

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,21 +1,22 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 
 
 def test_get_mask_card_number(number_card):
-    assert get_mask_card_number(number_card) == '7000 79** **** 6361'
+    assert get_mask_card_number(number_card) == "7000 79** **** 6361"
 
     with pytest.raises(TypeError):
         assert get_mask_card_number()
 
-    assert get_mask_card_number(234562) == None
+    assert get_mask_card_number(234562) is None
 
 
 def test_get_mask_account(account_number):
-    assert get_mask_account(account_number) == '**4305'
+    assert get_mask_account(account_number) == "**4305"
 
     with pytest.raises(TypeError):
         assert get_mask_account()
 
-    assert get_mask_account('werhvbdgffdjbsth') == None
+    assert get_mask_account("werhvbdgffdjbsth") is None

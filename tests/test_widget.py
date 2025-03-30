1802c6d11c3f8daace6d5c4e4a5c4e7872d03e2b
Игорь <Zabashtaigor@gmail.com>
2025-03-30 19:17:41 +0300

    Прогрес домашки 10.2

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,22 +1,25 @@
 import pytest
 
-from src.widget import get_date, mask_account_card
+from src.widget import get_date
+from src.widget import mask_account_card
 
 
 def test_get_date(date_string):
-    assert get_date(date_string) == '11.03.2024'
+    assert get_date(date_string) == "11.03.2024"
 
 
-@pytest.mark.parametrize('string, expected', [
-    ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
-    ('Счет 64686473678894779589', 'Счет **9589'),
-    ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
-    ('Счет 35383033474447895560', 'Счет **5560'),
-    ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
-    ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
-    ('Visa Gold 5999414228426353', 'Visa Gold 5999 41** **** 6353'),
-    ('Счет 73654108430135874305', 'Счет **4305')
-])
-
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
 def test_mask_account_card(string, expected):
     assert mask_account_card(string) == expected

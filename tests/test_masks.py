f7cf1bac8101e60234b30c30ade1cfc231df9bd2
Игорь <Zabashtaigor@gmail.com>
2025-04-01 22:02:00 +0300

    в README.md добавлен раздел - Тестирование

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,7 +4,7 @@ from src.masks import get_mask_account
 from src.masks import get_mask_card_number
 
 
-def test_get_mask_card_number(number_card):
+def test_get_mask_card_number(number_card: str) -> None:
     assert get_mask_card_number(number_card) == "7000 79** **** 6361"
 
     with pytest.raises(TypeError):
@@ -13,7 +13,7 @@ def test_get_mask_card_number(number_card):
     assert get_mask_card_number(234562) is None
 
 
-def test_get_mask_account(account_number):
+def test_get_mask_account(account_number: str) -> None:
     assert get_mask_account(account_number) == "**4305"
 
     with pytest.raises(TypeError):
